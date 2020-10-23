113abc3124a1f2e53fb2ab9058101a161818b037
Colin Osterhout <ctosterhout@alaska.edu>
2020-10-22 20:32:11 -0800

    Now loads one profile. Using the useFetch functionality of react-async instead of the homegrown jqXHR variant from jQuery.

--- src/App.js
+++ src/App.js
@@ -1,71 +1,44 @@
 import React from 'react';
-import './App.css';
-import $ from 'jquery';
-import _ from 'lodash';
-import Async from 'react-async'
-import DataTable from 'react-data-table'
+import 'milligram';
+import { useFetch } from 'react-async';
 
-const TIMEOUT = 5000;
-const urlSlackWidgetProxy = 'https://wg2ljjg5da.execute-api.us-east-1.amazonaws.com/dev/profile/view';
+const urlSlackProfileView = 'https://wg2ljjg5da.execute-api.us-east-1.amazonaws.com/dev/profile/view';
 
-// ----------------------------------------------------------------------------
-// loadProfile - bring in a profile from the AKDevAlliance slack channel
-//
-// Returns Promise which resolves with the data obtained from the URL or rejected
-// with an Error object.
-//
-// Utilizes lodash's memoize function to cache the result so that only one fetch
-// is made to the same URL.
-// ----------------------------------------------------------------------------
-const loadProfile = _.memoize(profileId => {
-  return new Promise((resolve, reject) => {
-    // Set a timeout so that we can provide a graceful way to report network issues
-    const hTimeout = setTimeout(() => {
-      reject(new Error('Timeout loading profile data'))
-    }, TIMEOUT)
+function ProfileView(props) {
+  const headers = { Accept: 'application/json' };
+  const options = { method: 'GET', mode: 'cors' };
+  const { data, error, isPending, run } = useFetch(`${urlSlackProfileView}/${props.idProfile}`, { headers }, options);
+  
+  if (data) {
+    return (
+      <div className="profile">
+        <h2>{data.real_name}</h2>
+        <img src={data.image_512} alt={`Profile picture for ${data.real_name}`}/>
+      </div>
+    );
+  }
+  
+  if (error) {
+    return (
+      <pre>{JSON.stringify(error, null, 2)}</pre>
+    );
+  }
+  
+  return null;
+}
 
-    const isErrorCritical = jqXHR => !(jqXHR.readyState === 0 && jqXHR.status === 0)
-    const processData = (data) => {
-      // perform any necessary data mangling here
-      clearTimeout(hTimeout)
-      resolve(data)
-    }
+// Stubbed out for now
+function ProfileList(props) {
+  return null;
+}
 
-    // Use jQuery to get the desired data and then upon receipt run the processData function to massage into usable data
-    $.get({
-      url: urlSlackWidgetProxy,
-      dataType: 'json'
-    }).done(processData)
-      .fail((jqXHR, textStatus) => {
-        if (isErrorCritical(jqXHR)) {
-          reject(new Error(`Could not load profile data. Status: ${textStatus}`))
-        }
-      })
-  })
-})
-
+// Presently justs lists yours truly
 function App() {
   return (
-    <h2>List of Profiles</h2>
-    <Async promiseFn={loadProfiles}>
-      <DataTable data={dataProfiles}></DataTable>
-    </Async>
-    
-    <h2>Individual Sample Profile</h2>
-    <Async promiseFn={loadProfile}>
-      {({ data, error, isLoading }) => {
-        if (isLoading) return "Loading..."
-        if (error) return `Something went wrong: ${error.message}`
-        if (data)
-          return (
-            <div>
-              <strong>Loaded some data:</strong>
-              <pre>{JSON.stringify(data, null, 2)}</pre>
-            </div>
-          )
-        return null
-      }}
-    </Async>
+    <div className="container">
+      <h2>List of Profiles</h2>
+      <ProfileView idProfile='UT1AQMNHK'/>
+    </div>
   );
 }
 export default App;
